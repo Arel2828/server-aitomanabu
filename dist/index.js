0aa25b683c07a010c95aa823afc1c0f9d5ba0b56
Arel2828 <arel062805@gmail.com>
2025-01-17 23:05:03 +0800

    vercel.json

--- dist/index.js
+++ dist/index.js
@@ -81,6 +81,7 @@ app.use(helmet_1.default.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use((0, morgan_1.default)("common"));
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.options("", (0, cors_1.default)(corsConfig));
 app.use((0, cors_1.default)(corsConfig));
 app.use((0, express_2.clerkMiddleware)());
 //Routes
